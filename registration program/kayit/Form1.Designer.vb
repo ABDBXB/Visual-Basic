afe810deb8e13e5069e6df73b19aced788cdec58
ABDBXB <66337063+ABDBXB@users.noreply.github.com>
2021-07-06 10:29:59 +0300

    Upload Registration Program

---
+++ registration program/kayit/Form1.Designer.vb
@@ -0,0 +1,231 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
+        Me.TextBox1 = New System.Windows.Forms.TextBox()
+        Me.TextBox2 = New System.Windows.Forms.TextBox()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.Button2 = New System.Windows.Forms.Button()
+        Me.DataGridView1 = New System.Windows.Forms.DataGridView()
+        Me.ContextMenuStrip1 = New System.Windows.Forms.ContextMenuStrip(Me.components)
+        Me.SilToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.Button3 = New System.Windows.Forms.Button()
+        Me.Button4 = New System.Windows.Forms.Button()
+        Me.Button5 = New System.Windows.Forms.Button()
+        Me.Button6 = New System.Windows.Forms.Button()
+        Me.DateTimePicker1 = New System.Windows.Forms.DateTimePicker()
+        Me.Label4 = New System.Windows.Forms.Label()
+        CType(Me.DataGridView1, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.ContextMenuStrip1.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'TextBox1
+        '
+        Me.TextBox1.Location = New System.Drawing.Point(96, 26)
+        Me.TextBox1.MaxLength = 11
+        Me.TextBox1.Name = "TextBox1"
+        Me.TextBox1.Size = New System.Drawing.Size(100, 20)
+        Me.TextBox1.TabIndex = 0
+        '
+        'TextBox2
+        '
+        Me.TextBox2.Location = New System.Drawing.Point(96, 68)
+        Me.TextBox2.Name = "TextBox2"
+        Me.TextBox2.Size = New System.Drawing.Size(100, 20)
+        Me.TextBox2.TabIndex = 1
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(5, 29)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(24, 13)
+        Me.Label1.TabIndex = 3
+        Me.Label1.Text = "T.C"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(5, 73)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(56, 13)
+        Me.Label2.TabIndex = 4
+        Me.Label2.Text = "Ad Soyad:"
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(5, 117)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(58, 13)
+        Me.Label3.TabIndex = 5
+        Me.Label3.Text = "Doğum Yıl:"
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(274, 29)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(75, 36)
+        Me.Button1.TabIndex = 6
+        Me.Button1.Text = "Kayıt Ekle"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'Button2
+        '
+        Me.Button2.Location = New System.Drawing.Point(274, 102)
+        Me.Button2.Name = "Button2"
+        Me.Button2.Size = New System.Drawing.Size(92, 35)
+        Me.Button2.TabIndex = 7
+        Me.Button2.Text = "Kayıt Güncelle"
+        Me.Button2.UseVisualStyleBackColor = True
+        '
+        'DataGridView1
+        '
+        Me.DataGridView1.Anchor = CType(((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Left) _
+            Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.DataGridView1.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill
+        Me.DataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize
+        Me.DataGridView1.ContextMenuStrip = Me.ContextMenuStrip1
+        Me.DataGridView1.Location = New System.Drawing.Point(8, 196)
+        Me.DataGridView1.Name = "DataGridView1"
+        Me.DataGridView1.Size = New System.Drawing.Size(466, 150)
+        Me.DataGridView1.TabIndex = 8
+        '
+        'ContextMenuStrip1
+        '
+        Me.ContextMenuStrip1.Items.AddRange(New System.Windows.Forms.ToolStripItem() {Me.SilToolStripMenuItem})
+        Me.ContextMenuStrip1.Name = "ContextMenuStrip1"
+        Me.ContextMenuStrip1.Size = New System.Drawing.Size(87, 26)
+        '
+        'SilToolStripMenuItem
+        '
+        Me.SilToolStripMenuItem.Name = "SilToolStripMenuItem"
+        Me.SilToolStripMenuItem.Size = New System.Drawing.Size(86, 22)
+        Me.SilToolStripMenuItem.Text = "Sil"
+        '
+        'Button3
+        '
+        Me.Button3.Location = New System.Drawing.Point(8, 163)
+        Me.Button3.Name = "Button3"
+        Me.Button3.Size = New System.Drawing.Size(74, 27)
+        Me.Button3.TabIndex = 9
+        Me.Button3.Text = "İlk Kayıt"
+        Me.Button3.UseVisualStyleBackColor = True
+        '
+        'Button4
+        '
+        Me.Button4.Location = New System.Drawing.Point(88, 163)
+        Me.Button4.Name = "Button4"
+        Me.Button4.Size = New System.Drawing.Size(74, 27)
+        Me.Button4.TabIndex = 10
+        Me.Button4.Text = "Son Kayıt"
+        Me.Button4.UseVisualStyleBackColor = True
+        '
+        'Button5
+        '
+        Me.Button5.Location = New System.Drawing.Point(274, 163)
+        Me.Button5.Name = "Button5"
+        Me.Button5.Size = New System.Drawing.Size(74, 27)
+        Me.Button5.TabIndex = 11
+        Me.Button5.Text = "Önceki"
+        Me.Button5.UseVisualStyleBackColor = True
+        '
+        'Button6
+        '
+        Me.Button6.Location = New System.Drawing.Point(354, 163)
+        Me.Button6.Name = "Button6"
+        Me.Button6.Size = New System.Drawing.Size(74, 27)
+        Me.Button6.TabIndex = 12
+        Me.Button6.Text = "Sonraki"
+        Me.Button6.UseVisualStyleBackColor = True
+        '
+        'DateTimePicker1
+        '
+        Me.DateTimePicker1.CustomFormat = "dd/MM/yyyy"
+        Me.DateTimePicker1.Format = System.Windows.Forms.DateTimePickerFormat.Custom
+        Me.DateTimePicker1.Location = New System.Drawing.Point(88, 111)
+        Me.DateTimePicker1.Name = "DateTimePicker1"
+        Me.DateTimePicker1.Size = New System.Drawing.Size(120, 20)
+        Me.DateTimePicker1.TabIndex = 14
+        Me.DateTimePicker1.Value = New Date(2021, 4, 30, 0, 0, 0, 0)
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Font = New System.Drawing.Font("Times New Roman", 14.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label4.Location = New System.Drawing.Point(206, 164)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(35, 22)
+        Me.Label4.TabIndex = 15
+        Me.Label4.Text = "0/0"
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(486, 450)
+        Me.Controls.Add(Me.Label4)
+        Me.Controls.Add(Me.DateTimePicker1)
+        Me.Controls.Add(Me.Button6)
+        Me.Controls.Add(Me.Button5)
+        Me.Controls.Add(Me.Button4)
+        Me.Controls.Add(Me.Button3)
+        Me.Controls.Add(Me.DataGridView1)
+        Me.Controls.Add(Me.Button2)
+        Me.Controls.Add(Me.Button1)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.TextBox2)
+        Me.Controls.Add(Me.TextBox1)
+        Me.Name = "Form1"
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
+        Me.Text = "Form1"
+        CType(Me.DataGridView1, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ContextMenuStrip1.ResumeLayout(False)
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents TextBox1 As TextBox
+    Friend WithEvents TextBox2 As TextBox
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Label3 As Label
+    Friend WithEvents Button1 As Button
+    Friend WithEvents Button2 As Button
+    Friend WithEvents DataGridView1 As DataGridView
+    Friend WithEvents Button3 As Button
+    Friend WithEvents Button4 As Button
+    Friend WithEvents Button5 As Button
+    Friend WithEvents Button6 As Button
+    Friend WithEvents ContextMenuStrip1 As ContextMenuStrip
+    Friend WithEvents SilToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents DateTimePicker1 As DateTimePicker
+    Friend WithEvents Label4 As Label
+End Class
---
+++ registration program/kayit/Form1.vb
@@ -0,0 +1,224 @@
+﻿Imports System.Data
+Imports System.Data.SqlClient
+
+Public Class Form1
+    Dim baglanti As New SqlConnection("Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=E:\IIII Donem\Görsel Programlama II\kayit\kayit\kayit\Database1.mdf;Integrated Security=True")
+    Dim verioku As SqlDataReader
+    Dim cm As CurrencyManager
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        'DateTimePicker1.Format = DateTimePickerFormat.Custom
+        'DateTimePicker1.CustomFormat = "dd/MM/yyyy"
+        goster()
+        Currencyayarla()
+
+        '==============
+        'Data Tabel
+        '==============
+        'Dim Dataadapter As New SqlDataAdapter("select tc,adsoyad,d_yili from kayit ", baglanti)
+        'baglanti.Open()
+        'Dataadapter.Fill(dt)
+        'baglanti.Close()
+
+        'TextBox1.DataBindings.Add("Text", dt, "tc")
+        'TextBox2.DataBindings.Add("Text", dt, "adsoyad")
+        'DateTimePicker1.DataBindings.Add("Value", dt, "d_yili")
+
+        'cm = Me.BindingContext(dt)
+        'Label4.Text = "1" + "/" + cm.Count.ToString()
+        '------------------------------------------------
+
+
+    End Sub
+    Public Sub Currencyayarla()
+
+
+        'Application.Restart()
+        Dim Dataadapter As New SqlDataAdapter("select tc,adsoyad,d_yili from kayit ", baglanti)
+        baglanti.Open()
+        Dim dt As New DataTable
+        Dataadapter.Fill(dt)
+        baglanti.Close()
+
+        TextBox1.DataBindings.Clear()
+        TextBox2.DataBindings.Clear()
+        DateTimePicker1.DataBindings.Clear()
+
+        TextBox1.DataBindings.Add("Text", dt, "tc")
+        TextBox2.DataBindings.Add("Text", dt, "adsoyad")
+        DateTimePicker1.DataBindings.Add("Value", dt, "d_yili")
+
+        'cm.ResumeBinding()
+
+        cm = Me.BindingContext(dt)
+        Try
+
+            cm.Refresh()
+            'cm.Position = cm.Count
+
+        Catch ex As Exception
+        End Try
+
+        Label4.Text = "1" + "/" + cm.Count.ToString()
+        'Label4.Text = (cm.Count).ToString() + "/" + cm.Count.ToString()
+    End Sub
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Dim tc, adsoyad, d_yili As String
+        tc = TextBox1.Text
+        adsoyad = TextBox2.Text
+        d_yili = DateTimePicker1.Value.ToString("MM/dd/yyyy")
+
+        If tc.Equals("") Or adsoyad.Equals("") Then
+            MsgBox("alanlar boş geçirlemez")
+
+        Else
+            Dim sqlquary As String = "select * from Kayit where tc='" & tc & "' "
+        Dim sqlkomut As New SqlCommand(sqlquary, baglanti)
+            baglanti.Open()
+            verioku = sqlkomut.ExecuteReader
+            If verioku.HasRows Then
+                MsgBox("Zaten Kayıtlı", MsgBoxStyle.Exclamation, "Uyarı")
+                baglanti.Close()
+            Else
+                baglanti.Close()
+                'INSERT KOMUTU
+                Dim sqlinsert As String = "insert into Kayit(tc,adsoyad,d_yili) values ('" & tc & "','" & adsoyad & "','" & d_yili & "')"
+                Dim insertcommand As New SqlCommand(sqlinsert, baglanti)
+                baglanti.Open()
+                insertcommand.ExecuteNonQuery()
+                baglanti.Close()
+                Currencyayarla()
+                goster()
+                MsgBox("Kişi Başarılı bir Şekilde Kaydedildi", MsgBoxStyle.Information, "Bilgilendirme")
+                Button5.Enabled = True
+                Button6.Enabled = True
+            End If
+        End If
+
+
+
+
+
+
+
+    End Sub
+
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        Dim tc, adsoyad, d_yili As String
+        tc = TextBox1.Text
+        adsoyad = TextBox2.Text
+        If tc.Equals("") Or adsoyad.Equals("") Then
+            MsgBox("alanlar boş geçirlemez")
+
+        Else
+            Dim sqlquary As String = "select * from Kayit where tc='" & tc & "' "
+            Dim sqlkomut As New SqlCommand(sqlquary, baglanti)
+                baglanti.Open()
+                verioku = sqlkomut.ExecuteReader
+            If verioku.HasRows Then
+                If (baglanti.State = ConnectionState.Open) Then
+                    baglanti.Close()
+                End If
+                tc = TextBox1.Text
+                adsoyad = TextBox2.Text
+                d_yili = DateTimePicker1.Value.ToString("MM/dd/yyyy")
+
+                Dim sqlupdate As String = "update Kayit set  adsoyad='" & adsoyad & "', d_yili='" & d_yili & "' where tc='" & tc & "' "
+                Dim update As New SqlCommand(sqlupdate, baglanti)
+                baglanti.Open()
+                update.ExecuteNonQuery()
+                baglanti.Close()
+                Currencyayarla()
+                goster()
+                MsgBox("Kişi Başarılı bir Şekilde güncellendi", MsgBoxStyle.Information, "Bilgilendirme")
+                Button5.Enabled = True
+                Button6.Enabled = True
+            Else
+                MsgBox("Böyle Bir Kişi Bulunmadı Lütfen Kaydedin sonra tekrar deneyiniz", MsgBoxStyle.Exclamation, "Uyarı")
+                baglanti.Close()
+
+
+            End If
+
+
+        End If
+
+
+
+    End Sub
+
+
+    Private Sub SilToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles SilToolStripMenuItem.Click
+        Dim tc As String
+        tc = DataGridView1.Rows(DataGridView1.CurrentCell.RowIndex).Cells("tc").Value
+        Dim komut As New SqlCommand("delete  from Kayit where tc='" & tc & "'", baglanti)
+        baglanti.Open()
+        komut.ExecuteNonQuery()
+        baglanti.Close()
+        MsgBox("Kişi Başarılı bir Şekilde Silindi", MsgBoxStyle.Information, "Bilgilendirme")
+        Currencyayarla()
+        goster()
+        Button5.Enabled = True
+        Button6.Enabled = True
+    End Sub
+
+
+    Public Sub goster()
+        Dim sql As String
+        sql = "select tc,adsoyad,d_yili from Kayit "
+        Dim komut As New SqlDataAdapter(sql, baglanti)
+        Dim verikumesi As New DataSet
+        baglanti.Open()
+        komut.Fill(verikumesi)
+        baglanti.Close()
+        DataGridView1.DataSource = verikumesi.Tables(0)
+        DataGridView1.Columns(0).HeaderText = "T.C."
+        DataGridView1.Columns(1).HeaderText = "AD Soyad"
+        DataGridView1.Columns(2).HeaderText = "Doğum Yılı"
+
+
+
+    End Sub
+
+
+
+
+    ''ilk kayit
+    Private Sub Button3_Click(sender As Object, e As EventArgs) Handles Button3.Click
+        cm.Position = 0
+        Label4.Text = (cm.Position + 1).ToString() + "/" + cm.Count.ToString()
+
+    End Sub
+    ''Son kayit
+    Private Sub Button4_Click(sender As Object, e As EventArgs) Handles Button4.Click
+        cm.Position = cm.Count - 1
+        Label4.Text = (cm.Position + 1).ToString() + "/" + cm.Count.ToString()
+    End Sub
+
+    Private Sub Button5_Click(sender As Object, e As EventArgs) Handles Button5.Click
+        cm.Position -= 1
+        Label4.Text = (cm.Position + 1).ToString() + "/" + cm.Count.ToString()
+        If cm.Position = 0 Then
+            Button5.Enabled = False
+            'MsgBox("Şimdi İlk Kayıt'tasiniz. ", MsgBoxStyle.Information, "Bilgilendirme")
+        Else
+            Button5.Enabled = True
+            Button6.Enabled = True
+        End If
+    End Sub
+
+    Private Sub Button6_Click(sender As Object, e As EventArgs) Handles Button6.Click
+        cm.Position += 1
+        Label4.Text = (cm.Position + 1).ToString() + "/" + cm.Count.ToString()
+        If cm.Position = cm.Count - 1 Then
+            Button6.Enabled = False
+            'MsgBox("Şimdi Son Kayıt'tasiniz. ", MsgBoxStyle.Information, "Bilgilendirme")
+        Else
+            Button6.Enabled = True
+            Button5.Enabled = True
+
+        End If
+    End Sub
+End Class
