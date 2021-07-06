ec5face1fb9b780c2dcb358d4c2ed7afd0c1c61c
ABDBXB <66337063+ABDBXB@users.noreply.github.com>
2021-07-06 10:28:16 +0300

    add Car Parking Program

---
+++ car parking program/araclar program/araclar program/Form1.Designer.vb
@@ -0,0 +1,264 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.ComboBox1 = New System.Windows.Forms.ComboBox()
+        Me.TblMarkaBindingSource = New System.Windows.Forms.BindingSource(Me.components)
+        Me.Database1DataSet = New araclar_program.Database1DataSet()
+        Me.ComboBox2 = New System.Windows.Forms.ComboBox()
+        Me.DataGridView1 = New System.Windows.Forms.DataGridView()
+        Me.ContextMenuStrip1 = New System.Windows.Forms.ContextMenuStrip(Me.components)
+        Me.SilToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.DüzenleToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.TextBox1 = New System.Windows.Forms.TextBox()
+        Me.ComboBox3 = New System.Windows.Forms.ComboBox()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.TextBox2 = New System.Windows.Forms.TextBox()
+        Me.Button2 = New System.Windows.Forms.Button()
+        Me.TblMarkaTableAdapter = New araclar_program.Database1DataSetTableAdapters.tblMarkaTableAdapter()
+        Me.PictureBox1 = New System.Windows.Forms.PictureBox()
+        Me.PictureBox2 = New System.Windows.Forms.PictureBox()
+        CType(Me.TblMarkaBindingSource, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.Database1DataSet, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.DataGridView1, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.ContextMenuStrip1.SuspendLayout()
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.PictureBox2, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(318, 94)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(75, 23)
+        Me.Button1.TabIndex = 0
+        Me.Button1.Text = "Kaydet"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'ComboBox1
+        '
+        Me.ComboBox1.DataSource = Me.TblMarkaBindingSource
+        Me.ComboBox1.DisplayMember = "Marka_ad"
+        Me.ComboBox1.FormattingEnabled = True
+        Me.ComboBox1.Location = New System.Drawing.Point(109, 90)
+        Me.ComboBox1.Name = "ComboBox1"
+        Me.ComboBox1.Size = New System.Drawing.Size(121, 21)
+        Me.ComboBox1.TabIndex = 1
+        '
+        'TblMarkaBindingSource
+        '
+        Me.TblMarkaBindingSource.DataMember = "tblMarka"
+        Me.TblMarkaBindingSource.DataSource = Me.Database1DataSet
+        '
+        'Database1DataSet
+        '
+        Me.Database1DataSet.DataSetName = "Database1DataSet"
+        Me.Database1DataSet.SchemaSerializationMode = System.Data.SchemaSerializationMode.IncludeSchema
+        '
+        'ComboBox2
+        '
+        Me.ComboBox2.FormattingEnabled = True
+        Me.ComboBox2.Location = New System.Drawing.Point(109, 132)
+        Me.ComboBox2.Name = "ComboBox2"
+        Me.ComboBox2.Size = New System.Drawing.Size(121, 21)
+        Me.ComboBox2.TabIndex = 2
+        '
+        'DataGridView1
+        '
+        Me.DataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize
+        Me.DataGridView1.ContextMenuStrip = Me.ContextMenuStrip1
+        Me.DataGridView1.Location = New System.Drawing.Point(12, 220)
+        Me.DataGridView1.Name = "DataGridView1"
+        Me.DataGridView1.ReadOnly = True
+        Me.DataGridView1.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect
+        Me.DataGridView1.Size = New System.Drawing.Size(463, 166)
+        Me.DataGridView1.TabIndex = 3
+        '
+        'ContextMenuStrip1
+        '
+        Me.ContextMenuStrip1.Items.AddRange(New System.Windows.Forms.ToolStripItem() {Me.SilToolStripMenuItem, Me.DüzenleToolStripMenuItem})
+        Me.ContextMenuStrip1.Name = "ContextMenuStrip1"
+        Me.ContextMenuStrip1.Size = New System.Drawing.Size(117, 48)
+        '
+        'SilToolStripMenuItem
+        '
+        Me.SilToolStripMenuItem.Name = "SilToolStripMenuItem"
+        Me.SilToolStripMenuItem.Size = New System.Drawing.Size(116, 22)
+        Me.SilToolStripMenuItem.Text = "Sil"
+        '
+        'DüzenleToolStripMenuItem
+        '
+        Me.DüzenleToolStripMenuItem.Name = "DüzenleToolStripMenuItem"
+        Me.DüzenleToolStripMenuItem.Size = New System.Drawing.Size(116, 22)
+        Me.DüzenleToolStripMenuItem.Text = "Düzenle"
+        '
+        'TextBox1
+        '
+        Me.TextBox1.Location = New System.Drawing.Point(109, 42)
+        Me.TextBox1.Name = "TextBox1"
+        Me.TextBox1.Size = New System.Drawing.Size(121, 20)
+        Me.TextBox1.TabIndex = 4
+        '
+        'ComboBox3
+        '
+        Me.ComboBox3.FormattingEnabled = True
+        Me.ComboBox3.Items.AddRange(New Object() {"Kırmızı", "Sarı", "Mavi", "Turuncu", "Yeşil", "Mor", "Siyah ", "Beyaz", "Gri"})
+        Me.ComboBox3.Location = New System.Drawing.Point(109, 168)
+        Me.ComboBox3.Name = "ComboBox3"
+        Me.ComboBox3.Size = New System.Drawing.Size(121, 21)
+        Me.ComboBox3.TabIndex = 5
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(45, 46)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(34, 13)
+        Me.Label1.TabIndex = 6
+        Me.Label1.Text = "Plaka"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(46, 94)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(37, 13)
+        Me.Label2.TabIndex = 7
+        Me.Label2.Text = "Marka"
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(46, 136)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(36, 13)
+        Me.Label3.TabIndex = 8
+        Me.Label3.Text = "Model"
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Location = New System.Drawing.Point(46, 172)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(33, 13)
+        Me.Label4.TabIndex = 9
+        Me.Label4.Text = "Renk"
+        '
+        'TextBox2
+        '
+        Me.TextBox2.Location = New System.Drawing.Point(12, 200)
+        Me.TextBox2.Name = "TextBox2"
+        Me.TextBox2.Size = New System.Drawing.Size(121, 20)
+        Me.TextBox2.TabIndex = 10
+        '
+        'Button2
+        '
+        Me.Button2.Location = New System.Drawing.Point(318, 132)
+        Me.Button2.Name = "Button2"
+        Me.Button2.Size = New System.Drawing.Size(75, 23)
+        Me.Button2.TabIndex = 11
+        Me.Button2.Text = "Güncelle"
+        Me.Button2.UseVisualStyleBackColor = True
+        '
+        'TblMarkaTableAdapter
+        '
+        Me.TblMarkaTableAdapter.ClearBeforeFill = True
+        '
+        'PictureBox1
+        '
+        Me.PictureBox1.Image = Global.araclar_program.My.Resources.Resources.add
+        Me.PictureBox1.Location = New System.Drawing.Point(248, 84)
+        Me.PictureBox1.Name = "PictureBox1"
+        Me.PictureBox1.Size = New System.Drawing.Size(28, 27)
+        Me.PictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom
+        Me.PictureBox1.TabIndex = 12
+        Me.PictureBox1.TabStop = False
+        '
+        'PictureBox2
+        '
+        Me.PictureBox2.Image = Global.araclar_program.My.Resources.Resources.icons8_add_48px
+        Me.PictureBox2.Location = New System.Drawing.Point(248, 126)
+        Me.PictureBox2.Name = "PictureBox2"
+        Me.PictureBox2.Size = New System.Drawing.Size(28, 27)
+        Me.PictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom
+        Me.PictureBox2.TabIndex = 13
+        Me.PictureBox2.TabStop = False
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(487, 428)
+        Me.Controls.Add(Me.PictureBox2)
+        Me.Controls.Add(Me.PictureBox1)
+        Me.Controls.Add(Me.Button2)
+        Me.Controls.Add(Me.TextBox2)
+        Me.Controls.Add(Me.Label4)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.ComboBox3)
+        Me.Controls.Add(Me.TextBox1)
+        Me.Controls.Add(Me.DataGridView1)
+        Me.Controls.Add(Me.ComboBox2)
+        Me.Controls.Add(Me.ComboBox1)
+        Me.Controls.Add(Me.Button1)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        CType(Me.TblMarkaBindingSource, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.Database1DataSet, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.DataGridView1, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ContextMenuStrip1.ResumeLayout(False)
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.PictureBox2, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents Button1 As Button
+    Friend WithEvents ComboBox1 As ComboBox
+    Friend WithEvents ComboBox2 As ComboBox
+    Friend WithEvents DataGridView1 As DataGridView
+    Friend WithEvents TextBox1 As TextBox
+    Friend WithEvents ComboBox3 As ComboBox
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Label3 As Label
+    Friend WithEvents ContextMenuStrip1 As ContextMenuStrip
+    Friend WithEvents SilToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents Label4 As Label
+    Friend WithEvents TextBox2 As TextBox
+    Friend WithEvents DüzenleToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents Button2 As Button
+    Friend WithEvents Database1DataSet As Database1DataSet
+    Friend WithEvents TblMarkaBindingSource As BindingSource
+    Friend WithEvents TblMarkaTableAdapter As Database1DataSetTableAdapters.tblMarkaTableAdapter
+    Friend WithEvents PictureBox1 As PictureBox
+    Friend WithEvents PictureBox2 As PictureBox
+End Class
---
+++ car parking program/araclar program/araclar program/Form1.vb
@@ -0,0 +1,349 @@
+﻿Imports System.Data
+Imports System.Data.SqlClient
+Public Class Form1
+    Dim baglanti As New SqlConnection("Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=E:\IIII Donem\Görsel Programlama II\araclar programi\araclar program\araclar program\Database1.mdf;Integrated Security=True")
+    Dim verioku As SqlDataReader
+    Dim sqlquary As String
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        'TODO: This line of code loads data into the 'Database1DataSet.tblMarka' table. You can move, or remove it, as needed.
+        Me.TblMarkaTableAdapter.Fill(Me.Database1DataSet.tblMarka)
+        ComboBox1.Text = "Seçiniz..."
+        goster()
+
+
+
+    End Sub
+    Public Function checkcombobox(CB1 As String, CB2 As String) As Boolean
+        If (baglanti.State = ConnectionState.Open) Then
+            baglanti.Close()
+        End If
+        CB1 = ComboBox1.Text
+        CB2 = ComboBox2.Text
+        Dim sqlkomut As New SqlCommand("select tblModel.*,tblMarka.* from tblModel,tblMarka where tblModel.marka_no =tblMarka.Id and tblMarka.Marka_ad ='" & CB1 & "' and tblModel.Model_ad  ='" & CB2 & "'", baglanti)
+        baglanti.Open()
+        verioku = sqlkomut.ExecuteReader
+        If verioku.HasRows Then
+            baglanti.Close()
+            Return True
+
+        Else
+            baglanti.Close()
+            Return False
+        End If
+
+
+    End Function
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Dim plaka, marka, model, renk, sqlinsert As String
+        plaka = TextBox1.Text
+        marka = ComboBox1.Text
+        model = ComboBox2.Text
+        renk = ComboBox3.Text
+
+        If plaka.Equals("") Or marka.Equals("") Or model.Equals("") Or renk.Equals("") Then
+            MsgBox("alanlar boş geçirlemez")
+
+        Else
+            sqlquary = "select * from araclar where plaka='" & plaka & "' "
+
+            Dim sqlkomut As New SqlCommand(sqlquary, baglanti)
+            baglanti.Open()
+            verioku = sqlkomut.ExecuteReader
+            If verioku.HasRows Then
+                MsgBox("Bu Arac Zaten Kayıtlı", MsgBoxStyle.Exclamation, "Uyarı")
+                baglanti.Close()
+            Else
+                'Marka ve Model Kayıtlı 
+                If checkcombobox(marka, model) = True Then
+                    If (baglanti.State = ConnectionState.Open) Then
+                        baglanti.Close()
+                    End If
+                    'MODEL ID'Sİ ALMA KOMUTU
+                    Dim Model_id As New SqlCommand("select id from tblModel where Model_ad='" & ComboBox2.Text & "'", baglanti)
+                    baglanti.Open()
+                    verioku = Model_id.ExecuteReader
+                    verioku.Read()
+                    model = verioku("id")
+                    baglanti.Close()
+                    'INSERT KOMUTU
+                    sqlinsert = "insert into araclar(plaka,model_no,renk) values ('" & plaka & "','" & model & "','" & renk & "')"
+                    Dim komut As New SqlCommand(sqlinsert, baglanti)
+                    baglanti.Open()
+                    komut.ExecuteNonQuery()
+                    baglanti.Close()
+                    goster()
+                    MsgBox("Araç Başarılı bir Şekilde Kaydedildi", MsgBoxStyle.Information, "Bilgilendirme")
+
+                Else
+                    MsgBox("Böyle bir Marka Yada Model Yok Lütfen Girniz.", MsgBoxStyle.Information, "Bilgilendirme")
+
+                End If
+
+
+
+
+
+            End If
+
+
+        End If
+    End Sub
+
+    Private Sub TextBox2_TextChanged(sender As Object, e As EventArgs) Handles TextBox2.TextChanged
+        Dim sqlView As String
+        sqlView = "select araclar.plaka,araclar.renk ,tblModel.Model_ad ,tblMarka.Marka_ad  from araclar ,tblModel ,tblMarka where araclar.model_no=tblModel.Id and tblModel.marka_no =tblMarka.Id and  plaka like '%" & TextBox2.Text & "%'"
+        Dim komut As New SqlDataAdapter(sqlView, baglanti)
+        baglanti.Open()
+        Dim ds As New DataSet
+        komut.Fill(ds)
+        baglanti.Close()
+        DataGridView1.DataSource = ds.Tables(0)
+
+    End Sub
+
+    Private Sub SilToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles SilToolStripMenuItem.Click
+        Dim plaka As String
+        plaka = DataGridView1.Rows(DataGridView1.CurrentCell.RowIndex).Cells("plaka").Value
+        Dim komut As New SqlCommand("delete  from araclar where plaka='" & plaka & "'", baglanti)
+
+        baglanti.Open()
+        komut.ExecuteNonQuery()
+        baglanti.Close()
+        MsgBox("Araç Başarılı bir Şekilde Silindi", MsgBoxStyle.Information, "Bilgilendirme")
+        goster()
+
+    End Sub
+
+
+    Public Sub goster()
+        Dim sql As String
+        sql = "select araclar.plaka,araclar.renk ,tblModel.Model_ad ,tblMarka.Marka_ad  from araclar ,tblModel ,tblMarka where araclar.model_no=tblModel.Id and tblModel.marka_no =tblMarka.Id "
+        Dim komut As New SqlDataAdapter(sql, baglanti)
+        Dim verikumesi As New DataSet
+        baglanti.Open()
+        komut.Fill(verikumesi)
+        baglanti.Close()
+        'DataGridView1.Rows.Clear()
+        DataGridView1.DataSource = verikumesi.Tables(0)
+        DataGridView1.Columns(0).HeaderText = "Plaka"
+        DataGridView1.Columns(1).HeaderText = "Renk"
+        DataGridView1.Columns(2).HeaderText = "Model ADI"
+        DataGridView1.Columns(3).HeaderText = "Marka ADI"
+    End Sub
+
+
+
+    Private Sub DüzenleToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles DüzenleToolStripMenuItem.Click
+        Dim plaka As String
+        plaka = DataGridView1.Rows(DataGridView1.CurrentCell.RowIndex).Cells("plaka").Value.ToString
+        Dim sql As String
+        sql = "select araclar.plaka,araclar.renk ,tblModel.Model_ad ,tblMarka.Marka_ad  from araclar ,tblModel ,tblMarka where araclar.model_no=tblModel.Id and tblModel.marka_no =tblMarka.Id and plaka='" & plaka & "' "
+
+        Dim komut As New SqlDataAdapter(sql, baglanti)
+        baglanti.Open()
+        Dim verikumesi As New DataSet
+        komut.Fill(verikumesi)
+        baglanti.Close()
+        TextBox1.Text = verikumesi.Tables(0).Rows(0)("plaka").ToString
+        ComboBox1.Text = verikumesi.Tables(0).Rows(0)("Marka_ad").ToString()
+        ComboBox2.Text = verikumesi.Tables(0).Rows(0)("Model_ad").ToString()
+        ComboBox3.Text = verikumesi.Tables(0).Rows(0)("renk").ToString()
+
+
+
+    End Sub
+
+    'Güncelle Button
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        Dim plaka, marka, model, renk, sqlupdate As String
+        plaka = TextBox1.Text
+        marka = ComboBox1.Text
+        model = ComboBox2.Text
+        renk = ComboBox3.Text
+        If plaka.Equals("") Or marka.Equals("") Or model.Equals("") Or renk.Equals("") Then
+            MsgBox("alanlar boş geçirlemez")
+
+        Else
+            If checkcombobox(marka, model) = True Then
+                sqlquary = "select * from araclar where plaka='" & plaka & "'  "
+
+                Dim sqlkomut As New SqlCommand(sqlquary, baglanti)
+                baglanti.Open()
+                verioku = sqlkomut.ExecuteReader
+                If verioku.HasRows Then
+                    If (baglanti.State = ConnectionState.Open) Then
+                        baglanti.Close()
+                    End If
+
+
+                    'MODEL ID'Sİ ALMA KOMUTU
+                    Dim Model_id As New SqlCommand("select id from tblModel where Model_ad='" & ComboBox2.Text & "'", baglanti)
+                    baglanti.Open()
+                    verioku = Model_id.ExecuteReader
+                    verioku.Read()
+                    model = verioku("id")
+                    baglanti.Close()
+
+                    plaka = TextBox1.Text
+                    renk = ComboBox3.Text
+                    sqlupdate = "update araclar set plaka='" & plaka & "', model_no='" & model & "', renk='" & renk & "' where plaka='" & plaka & "' "
+                    Dim update As New SqlCommand(sqlupdate, baglanti)
+                    baglanti.Open()
+                    update.ExecuteNonQuery()
+                    baglanti.Close()
+                    goster()
+                    MsgBox("Araç Başarılı bir Şekilde güncellendi", MsgBoxStyle.Information, "Bilgilendirme")
+
+                Else
+                    MsgBox("Böyle Bir Araç Bulunmadı Lütfen Kaydedin sonra tekrar deneyiniz", MsgBoxStyle.Exclamation, "Uyarı")
+                    baglanti.Close()
+
+
+                End If
+
+            Else
+                MsgBox("Böyle bir Marka Yada Model Yok Lütfen Girniz.", MsgBoxStyle.Information, "Bilgilendirme")
+
+            End If
+
+
+
+
+        End If
+    End Sub
+
+    Private Sub DataGridView1_MouseDoubleClick(sender As Object, e As MouseEventArgs) Handles DataGridView1.MouseDoubleClick
+        Dim plaka As String
+        plaka = DataGridView1.Rows(DataGridView1.CurrentCell.RowIndex).Cells("plaka").Value.ToString
+        Dim sql As String
+        sql = "select araclar.plaka,araclar.renk ,tblModel.Model_ad ,tblMarka.Marka_ad  from araclar ,tblModel ,tblMarka where araclar.model_no=tblModel.Id and tblModel.marka_no =tblMarka.Id and plaka='" & plaka & "' "
+
+        Dim komut As New SqlDataAdapter(sql, baglanti)
+        baglanti.Open()
+        Dim verikumesi As New DataSet
+        komut.Fill(verikumesi)
+        baglanti.Close()
+        TextBox1.Text = verikumesi.Tables(0).Rows(0)("plaka").ToString
+        ComboBox1.Text = verikumesi.Tables(0).Rows(0)("Marka_ad").ToString()
+        ComboBox2.Text = verikumesi.Tables(0).Rows(0)("Model_ad").ToString()
+        ComboBox3.Text = verikumesi.Tables(0).Rows(0)("renk").ToString()
+    End Sub
+
+    Private Sub ComboBox1_SelectedIndexChanged(sender As Object, e As EventArgs) Handles ComboBox1.SelectedIndexChanged
+        ComboBox2.Items.Clear()
+        ComboBox2.Text = ""
+        Dim komut As New SqlCommand("select Model_ad  from tblModel where marka_no in (select id from tblMarka where marka_ad='" & ComboBox1.Text & "')", baglanti)
+        baglanti.Open()
+        verioku = komut.ExecuteReader
+        Do While verioku.Read
+            ComboBox2.Items.Add(verioku("Model_ad"))
+        Loop
+        If ComboBox2.Items.Count > 0 Then
+            ComboBox2.Text = ComboBox2.Items(0).ToString
+        End If
+        baglanti.Close()
+    End Sub
+
+    Private Sub PictureBox1_Click(sender As Object, e As EventArgs) Handles PictureBox1.Click
+
+        Dim marka_ad As String
+        marka_ad = InputBox("Yeni Markanın Adı Giriniz Lütfen", "Yeni Marka Ekleme")
+        If marka_ad.Equals("") Then
+            MsgBox("Marka Boş Girilimemeli.", MsgBoxStyle.Critical, "Bilgilendirme")
+        Else
+            Dim sqlkomut As New SqlCommand("select Marka_ad from tblMarka where Marka_ad='" & marka_ad & "'", baglanti)
+            baglanti.Open()
+            verioku = sqlkomut.ExecuteReader
+            If verioku.HasRows Then
+                MsgBox("Bu Marka Zaten Eklendi.", MsgBoxStyle.Information, "Bilgilendirme")
+                baglanti.Close()
+            Else
+
+                If (baglanti.State = ConnectionState.Open) Then
+                    baglanti.Close()
+                End If
+                Dim insert As New SqlCommand("insert into tblMarka(Marka_ad) values ('" & marka_ad & "')", baglanti)
+                baglanti.Open()
+                insert.ExecuteNonQuery()
+                baglanti.Close()
+
+                Dim komut As New SqlDataAdapter("select Marka_ad from tblMarka", baglanti)
+                Dim verikumesi As New DataSet
+                baglanti.Open()
+                komut.Fill(verikumesi)
+                baglanti.Close()
+                ComboBox1.DataSource = verikumesi.Tables(0)
+                ComboBox1.Text = marka_ad
+
+            End If
+
+
+
+
+            'Me.Controls.Clear()
+            'InitializeComponent()
+            'Form1_Load(e, e)
+            'Application.Restart()
+            'Me.TblMarkaTableAdapter.Fill(Me.Database1DataSet.tblMarka)
+            'Me.Refresh()
+
+
+
+        End If
+    End Sub
+
+    Private Sub PictureBox2_Click(sender As Object, e As EventArgs) Handles PictureBox2.Click
+        Dim Marka_id As Integer
+        Dim Marka_idsql As New SqlCommand("select id from tblMarka where Marka_ad='" & ComboBox1.Text & "'", baglanti)
+        baglanti.Open()
+        verioku = Marka_idsql.ExecuteReader
+        verioku.Read()
+        If verioku.HasRows Then
+            Marka_id = verioku("id")
+        End If
+        baglanti.Close()
+
+        If Marka_id = 0 Then
+            MsgBox("Lütfen Marka Seçin, Marka Yoksa ekleyin :)", MsgBoxStyle.Information, "Bilgilendirme")
+        Else
+            Dim Model_ad As String
+            Model_ad = InputBox("Yeni Modelin Adı Giriniz Lütfen", "Yeni Model Ekleme")
+            If Model_ad.Equals("") Then
+                MsgBox("Marka Boş Girilimemeli.", MsgBoxStyle.Critical, "Bilgilendirme")
+            Else
+
+                Dim sqlkomut As New SqlCommand("select Model_ad from tblModel where Model_ad='" & Model_ad & "'", baglanti)
+                baglanti.Open()
+                verioku = sqlkomut.ExecuteReader
+                If verioku.HasRows Then
+                    MsgBox("Bu Marka Zaten Eklendi.", MsgBoxStyle.Information, "Bilgilendirme")
+                    baglanti.Close()
+                Else
+                    If (baglanti.State = ConnectionState.Open) Then
+                        baglanti.Close()
+                    End If
+                    Dim insert As New SqlCommand("insert into tblModel(Model_ad,marka_no) values ('" & Model_ad & "','" & Marka_id & "')", baglanti)
+                    baglanti.Open()
+                    insert.ExecuteNonQuery()
+                    baglanti.Close()
+
+                    ComboBox2.Items.Clear()
+                    Dim komut As New SqlCommand("select Model_ad  from tblModel where marka_no in (select id from tblMarka where marka_ad='" & ComboBox1.Text & "')", baglanti)
+                    baglanti.Open()
+                    verioku = komut.ExecuteReader
+                    Do While verioku.Read
+                        ComboBox2.Items.Add(verioku("Model_ad"))
+                    Loop
+                    baglanti.Close()
+
+                End If
+
+
+
+
+            End If
+        End If
+
+    End Sub
+End Class
